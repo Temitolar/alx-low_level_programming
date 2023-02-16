c183eda44716bc35e14c871d02bae8b6c144ef94
Olokuntoye Temitola <olokuntoyetemitola@gmail.com>
2023-02-16 14:33:23 -0800

    UNIX

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * Main - A c program that prints a line to the standard error
- * Return 1 (sucess)
+ * main - A C program that prints a line to a standard error
+ * Return: 1 (success)
  */
 int main(void)
 {
-	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
-return (1);
+	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	return (1);
 }
