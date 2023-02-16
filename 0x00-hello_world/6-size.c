e29a644d4bc96f5a787b4f2c190f20d8acec0179
Olokuntoye Temitola <olokuntoyetemitola@gmail.com>
2023-02-16 04:17:49 -0800

    size

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - A program that prinys a line using the printf function
+ * Return: 0(success)
+ */
+int main(void)
+{
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float f;
+
+	printf("size of a char: %lu byte(s)\n",(unsigned long)size of (a));
+	printf("size of an int: %lu byte(s)\n",(unsigned long)size of (b));
+	printf("size of a long int: %lu byte(s)\n",(unsigned long)size of (c));
+	printf("size of a long long int: %lu byte(s)\n",(unsigned long)size of (d));
+	print("size of a float: %lu byte(s)\n",(unsigned long)size of (f));
+	return (0);
+}
