2ffd158f39a0d9f4b2720beaf5946fb0919dd93e
Olokuntoye Temitola <olokuntoyetemitola@gmail.com>
2023-02-21 13:16:21 -0800

    even liber

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - prints the sum of even fibonacci numbers
+ * less than 4000000.
+ * Return: nothing!
+ */
+
+int main(void)
+{
+
+	int  = 0;
+	long j = 1, k = 2, sum = k;
+
+	while (k + j < 4000000)
+	{
+	if k += j;
+	if (k % 2 == 0)
+	sum += k;
+	j = k - j;
+	++i;
+	}
+	print("%ld\n", sum);
+	return (0);
+}
