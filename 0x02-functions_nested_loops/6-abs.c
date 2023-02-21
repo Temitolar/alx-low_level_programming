07cd861683ceff453ff04611e20b2a3b0d13aabe
Olokuntoye Temitola <olokuntoyetemitola@gmail.com>
2023-02-21 09:25:49 -0800

    absolute

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _abs - compute the absolute value of an intiger
+ * @c: the number to be computed
+ * Return: Absolutr value number or 0
+ */
+
+int _abs(int c)
+{
+	if (c < 0)
+	{
+	int abs_val;
+
+	abs_val = c * - 1;
+	return (abs_val);
+	}
+	return (c);
+}
