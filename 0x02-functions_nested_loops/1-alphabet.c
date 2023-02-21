fae66e72b81af7170a6a9dfdbd7f80cde7988cf6
Olokuntoye Temitola <olokuntoyetemitola@gmail.com>
2023-02-21 06:58:43 -0800

    10 x alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,18 +2,20 @@
 
 /**
  * print_alphabet - make the alphabet
- * Return: void
+ * Return:void
  */
 
 void print_alphabet(void)
 {
 	char c;
+
 	for (c = 'a'; c <= 'z'; c++)
 	{
 	_putchar(c);
 	}
 	_putchar('\n');
 }
+	
 
 	
 
