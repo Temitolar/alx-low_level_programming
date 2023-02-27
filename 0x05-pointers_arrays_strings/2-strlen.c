e370a230e8a4ad45624490fb81c27a5035bc6042
Olokuntoye Temitola <olokuntoyetemitola@gmail.com>
2023-02-27 03:59:34 -0800

    2-strlen.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,19 +1,18 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
  * _strlen - returns the length of a string
  * @s: string
- * return:length
+ * Return: length
  *
  */
 
 int _strlen(char *s)
 {
-	
 	int longi = 0;
 
 	while (*s != '\0')
-	{	
+	{
 	longi++;
 	s++;
 	}
