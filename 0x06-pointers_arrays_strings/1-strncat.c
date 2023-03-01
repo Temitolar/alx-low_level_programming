4c3316430ad3cafbb727f45ed4d12cad1dfc9f5b
Olokuntoye Temitola <olokuntoyetemitola@gmail.com>
2023-03-01 06:53:38 -0800

    1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -7,16 +7,23 @@
  * @src: pointer to source string.
  * @n: number of bytes to be concatenated.
  * Return: pointer to destination string.
- * 
  */
 
 char *_strncat(char *dest, char *src, int n)
 {
-	int index = 0, dest_len = 0;
+	int length, j;
+	/* j is a counter for  n bytes of src to be concatenated */
+	/* length - length of destination string */
 
-	while (dest[index++])
-	dest_len++;
-	for (index = 0; src[index] && index < n; index++)
-	dest[dest_len++] = src[index];
+	length = 0;
+	while (dest[length] != '\0')
+	{
+		length++;
+	}
+	for (j = 0; j < n && src[j] != '\0'; j++, length++)
+	{
+		dest[length] = src[j];
+	}
+	dest[length] = '\0';
 	return (dest);
 }
