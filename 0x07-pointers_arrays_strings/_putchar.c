f065d7fef4ee7cf572f936a433880dd78f724dd2
Olokuntoye Temitola <olokuntoyetemitola@gmail.com>
2023-03-06 04:01:04 -0800

    0-memset.c

---
+++ 0x07-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,14 @@
+#include <unistd.h>
+
+/**
+ * _putvhar - write the character c to stdout
+ * @c: the character to be printed
+ *
+ * Return: 1 on success
+ * on error, -1 is returned, and error is set appropriately
+ */
+
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
