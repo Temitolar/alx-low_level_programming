f90e1487b866a84dfc63a38ddf76c889381ee60e
Olokuntoye Temitola <olokuntoyetemitola@gmail.com>
2023-03-08 01:32:46 -0800

    3-factorial.c

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * factorial - returns factors
+ * @n: number to factor
+ * Return: factor
+ */
+
+int factorial(int n)
+{
+	if (n == 0)
+	{
+		return (1);
+	}
+	else if (n < 0)
+	{
+		return (-1);
+	}
+	return (n * factorial(n - 1));
+}
