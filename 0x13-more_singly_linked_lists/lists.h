60dce832334b5588651a5e4e15ba2fd83c59ddb8
Olokuntoye Temitola <olokuntoyetemitola@gmail.com>
2023-04-04 09:34:23 -0700

    0-print_listint.c

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,13 +1,12 @@
-#ifndef LISTS_H
-#define LISTS_H
+#ifndef _LISTS_H_
+#define _LISTS_H_
 
-#include <stdio.h>
-#include <stdlib.h>
-
+#include <stddef.h>
 /**
  * struct listint_s - singly linked list
  * @n: integer
- * @next: points to the next list
+ * @next: points to the next node
+ *
  * Description: singly linked list node structure
  * for ALX project
  */
@@ -33,4 +32,4 @@ size_t print_listint_safe(const listint_t *head);
 size_t free_listint_safe(listint_t **h);
 listint_t *find_listint_loop(listint_t *head);
 
-#endif
+#endif /* _LISTS_H_ */
